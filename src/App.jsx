cf21252c08486b46c3a86e5511c55a0871cd7ac2
Bikila1 <binagi2009@gmail.com>
2024-03-05 09:15:03 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Stopwatch from "./Stopwatch";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
